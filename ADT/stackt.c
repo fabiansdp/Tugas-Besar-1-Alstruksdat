397f29b47cfa135654d8d5e7ffe3ba6c8dcbdeec
hafidabi <abi.daniswara@gmail.com>
2020-11-19 14:06:46 +0700

    pengadaan stack

---
+++ ADT/stackt.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include"stackt.h"
+#include"boolean.h"
+
+void CreateEmpty (Stack *S){
+    Top(*S) = Nil;
+    Time(*S) = 0;
+}
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty (Stack S){
+    return Top(S) == Nil;
+}
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+boolean IsFull (Stack S){
+    return Top(S) == MaxEl-1;
+}
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push (Stack * S, Perintah X){
+    if(!IsFull(*S)){
+        Top(*S)++;
+        InfoTop(*S) = X;
+        Time(*S) = Time(*S) + X.durasi;
+    }
+}
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop (Stack * S, Perintah * X){
+    if(!IsEmpty(*S)){
+        (*X) = InfoTop(*S);
+        Time(*S) = Time(*S) + InfoTop(*S).durasi;
+        Top(*S)--;
+        
+    }
+}
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
