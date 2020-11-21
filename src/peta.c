7ce66e3916cea3033ca1f75b96b0176c91fac29b
Purecon <mfurqon2001@gmail.com>
2020-11-21 21:12:26 +0700

    peta.c ditambah adjacency dan edit point

--- src/peta.c
+++ src/peta.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 //menggunakan adt matriks
-#include "matriks.h"
-// #include "matriks.c"
-#include "point.h"
-// #include "point.c"
+#include "../ADT/boolean.h"
+#include "../ADT/matriks.c"
+#include "../ADT/point.c"
 
 /* Kamus */
 MATRIKS L;
-POINT P[200];
+typedef struct {
+	POINT P[200];
+    int tipe[200];
+} Arr_POINT;
+Arr_POINT AP;
 int tipe_point[10][20];
 POINT player_loc; 
+
 // char L[10][20];
 /* Algoritma */
 void BacaPeta(int x, MATRIKS *L)
@@ -111,7 +115,7 @@ void TulisPeta(int x, MATRIKS L)
     fclose(fh);
 }
 
-void TitikPeta(MATRIKS L, POINT P[])
+void TitikPeta(MATRIKS L, Arr_POINT AP)
 {
     /* Kamus Lokal*/
     int i,j;
@@ -129,7 +133,8 @@ void TitikPeta(MATRIKS L, POINT P[])
             {
                 // printf("Antrian\n");
                 // P[(i*20)+j]=MakePOINT(i,j);
-                P[count]=MakePOINT(i,j);
+                AP.P[count]=MakePOINT(i,j);
+                AP.tipe[count]=1;
                 count++;
                 tipe_point[i][j]=1;
             }
@@ -137,35 +142,40 @@ void TitikPeta(MATRIKS L, POINT P[])
             {
                 printf("Player di titik %d,%d\n",i,j);
                 player_loc=MakePOINT(i,j);
-                P[count]=MakePOINT(i,j);
+                AP.P[count]=MakePOINT(i,j);
+                AP.tipe[count]=2;
                 count++;
                 tipe_point[i][j]=2;
             }
             else if(temp =='O')
             {
                 // printf("Office\n");
-                P[count]=MakePOINT(i,j);
+                AP.P[count]=MakePOINT(i,j);
+                AP.tipe[count]=3;
                 count++;
                 tipe_point[i][j]=3;
             }
             else if(temp =='V' || temp =='>' || temp =='<' || temp =='^')
             { 
                 // printf("Gerbang\n");
-                P[count]=MakePOINT(i,j);
+                AP.P[count]=MakePOINT(i,j);
+                AP.tipe[count]=4;
                 count++;
                 tipe_point[i][j]=4;
             }
             else if (temp == 'W')
             {
                 // printf("Wahana\n");
-                P[count]=MakePOINT(i,j);
+                AP.P[count]=MakePOINT(i,j);
+                AP.tipe[count]=5;
                 count++;
                 tipe_point[i][j]=5;
             }
             else if (temp == '*')
             {
                 // printf("Dinding\n");
-                P[count]=MakePOINT(i,j);
+                AP.P[count]=MakePOINT(i,j);
+                AP.tipe[count]=6;
                 count++;
                 tipe_point[i][j]=6;
             }
@@ -284,6 +294,28 @@ void PrintPeta(MATRIKS L)
     }
 }
 
+int Adjacency()
+{
+    /* Kamus Lokal */
+    int x,y;
+    /* Algoritma */
+    x=Absis(player_loc);
+    y=Ordinat(player_loc);
+    /*1.Antrian, 2.Player, 3.Office, 4.Gerbang, 5.Wahana, 6.Dinding*/
+    //sebelah wahana
+    if (tipe_point[x-1][y]==5 || tipe_point[x][y-1]==5 ||tipe_point[x+1][y]==5 || tipe_point[x][y+1]==5)
+    {
+        return(5);
+    }
+    //sebelah office
+    else if (tipe_point[x-1][y]==3 || tipe_point[x][y-1]==3 ||tipe_point[x+1][y]==3 || tipe_point[x][y+1]==3)
+    {
+        return(3);
+    }
+    //default 0
+    return 0;
+}
+
 //testing fungsi
 int main()
 {
@@ -291,6 +323,7 @@ int main()
     char input;
     int i,j;
     boolean loop;
+    int adjacent;
     //algoritma
     for (i = 0; i <= 9; i++)
     {
@@ -303,13 +336,14 @@ int main()
     MakeMATRIKS(10,20,&L);
     BacaPeta(1,&L);
     TulisPeta(1,L);
-    TitikPeta(L,P);
+    TitikPeta(L,AP);
     for (i = 0; i <= 199; i++)
     {
-        if (P[i].X!=0 && P[i].Y!=0)
+        if (AP.P[i].X!=0 && AP.P[i].Y!=0)
         {
-            printf("%f,",P[i].X);
-            printf("%f\n",P[i].Y);
+            printf("%d ",AP.tipe[i]);
+            printf("%f,",AP.P[i].X);
+            printf("%f\n",AP.P[i].Y);
         }           
         // printf("%f,",P[i].X);
         // printf("%f\n",P[i].Y);
@@ -317,10 +351,21 @@ int main()
 
     printf("X untuk keluar\n");
     loop=true;    
-    while (loop)
+    do
     {
+        adjacent=Adjacency();
+        printf("Nilai adjacent: %d\n",adjacent);
         printf("Masukkan arah pergerakan:\n");
         scanf("%c",&input);
+        if (adjacent==5)
+        {
+            printf("Sebelah wahana\n");
+        }
+        else if (adjacent==3)
+        {
+            printf("Sebelah office\n");
+        }
+
         if (input=='X')
         {
             loop=false;
@@ -330,7 +375,7 @@ int main()
             Movement(input,&L);
             PrintPeta(L);
         }
-    }
+    } while (loop);
 
     //cek tipe_point
     for (i = 0; i <= 9; i++)
