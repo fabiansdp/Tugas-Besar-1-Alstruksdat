a41b9d36dd35ea2730d85aa34550883792c039f0
Uyamikun <nabil.nabighah@gmail.com>
2020-11-24 21:51:20 +0700

    buy function, repair function

---
+++ src/buy.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../ADT/boolean.h"
+#include "../ADT/jam.h"
+#include "../ADT/command.h"
+#include "../ADT/arraypos.h"
+#include "../ADT/mesinkar.h"
+#include "../ADT/mesinkata.h"
+#define MAXCHAR 100
+
+//GLOBAL VARIABLE
+char mat [20][256];
+int banyak;
+TabEl T;
+Kata CKata,Air,Kayu,Batu,Besi;
+
+
+// HILANGIN ANGKA DARI FILE
+char* RemoveDigits(char* string)
+{
+    char* hasil = string;
+    char* before = string;
+
+    while(*before)
+    {
+        if (*before >= '0' && *before <= '9'){ 
+            before++; 
+            continue; 
+        }
+        if(*before == ' '){
+            before++; 
+            continue; 
+        }
+        *hasil++ = *before++;
+    }
+    *hasil = '\0';
+    return string;
+}
+
+// BACA HARGA
+void BacaHarga(int x,TabEl *T)
+{
+    FILE *fp;
+    char str[MAXCHAR];
+    int k;
+    int i =0;
+    switch(x)
+    {
+    case 1:
+        fp=fopen("material.txt","r");
+        break;
+    default:
+        fp=fopen("material.txt","r"); //asumsi file tidak kosong
+        break;
+    }
+    while (fgets(str,MAXCHAR,fp) != NULL){
+        //masukin harga ke array
+        k = atoi(str);
+        Id(*T,i) = i;
+        Value(*T,i) = k;
+        i++;
+    }
+    fclose(fp);
+}
+
+//BACA MATERIAL 
+void BacaMaterial(int x,char mat[20][256]){
+    FILE *fp;
+    char str[MAXCHAR];
+    int j,i = 0;
+    int k;
+    switch(x)
+    {
+    case 1:
+        fp=fopen("material.txt","r");
+        break;
+    default:
+        fp=fopen("material.txt","r"); //asumsi file tidak kosong
+        break;
+    }
+    while (fgets(str, sizeof(str), fp)){
+        RemoveDigits(str);
+        strcpy(mat[i], str);
+        printf("- %s",mat[i]);
+        i++;
+    }
+    printf("\n");
+    printf("format: mount material (ex: 10 besi)\n");
+    fclose(fp);
+}
+
+// BACA INPUT
+void BacaInput(){
+    printf("What do you want to buy? ");
+    scanf("%d",&banyak);
+    STARTKATA();
+    Air.TabKata[0] = 'a';
+    Air.TabKata[1] = 'i';
+    Air.TabKata[2] = 'r';
+    Air.Length = 3;
+    Kayu.TabKata[0] = 'k';
+    Kayu.TabKata[1] = 'a';
+    Kayu.TabKata[2] = 'y';
+    Kayu.TabKata[3] = 'u';
+    Kayu.Length = 4;
+    Batu.TabKata[0] = 'b';
+    Batu.TabKata[1] = 'a';
+    Batu.TabKata[2] = 't';
+    Batu.TabKata[3] = 'u';
+    Batu.Length = 4;
+    Besi.TabKata[0] = 'b';
+    Besi.TabKata[1] = 'e';
+    Besi.TabKata[2] = 's';
+    Besi.TabKata[3] = 'i';
+    Besi.Length = 4;
+    while(!IsKataSama(CKata,Air) && !IsKataSama(CKata,Kayu) && !IsKataSama(CKata,Batu) && !IsKataSama(CKata,Besi)){
+        printf("Material is not available, please try again\n");
+        printf("What do you want to buy? ");
+        scanf("%d",&banyak);
+        STARTKATA();
+    }
+}
+
+//CEK UANG CUKUP
+boolean EnoughMoney(int money,int banyak,TabEl T,Kata CKata){
+    int i;
+    Air.TabKata[0] = 'a';
+    Air.TabKata[1] = 'i';
+    Air.TabKata[2] = 'r';
+    Air.Length = 3;
+    Kayu.TabKata[0] = 'k';
+    Kayu.TabKata[1] = 'a';
+    Kayu.TabKata[2] = 'y';
+    Kayu.TabKata[3] = 'u';
+    Kayu.Length = 4;
+    Batu.TabKata[0] = 'b';
+    Batu.TabKata[1] = 'a';
+    Batu.TabKata[2] = 't';
+    Batu.TabKata[3] = 'u';
+    Batu.Length = 4;
+    Besi.TabKata[0] = 'b';
+    Besi.TabKata[1] = 'e';
+    Besi.TabKata[2] = 's';
+    Besi.TabKata[3] = 'i';
+    Besi.Length = 4;
+    if(IsKataSama(CKata,Air)){
+        i = 0;
+    }
+    if(IsKataSama(CKata,Kayu)){
+        i = 1;
+    }
+    if(IsKataSama(CKata,Batu)){
+        i = 2;
+    }
+    if(IsKataSama(CKata,Besi)){
+        i = 3;
+    }
+    if((money - banyak*Value(T,i)) < 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+int main()
+{
+    MakeEmpty(&T);
+    //Jam J;
+    //BacaJam(&J);
+    printf("Welcome to the shop\n ");
+    printf("Material List :\n");
+    BacaMaterial(1,mat);
+    BacaHarga(1,&T);
+    BacaInput();
+    boolean x;
+    x =EnoughMoney(1000,banyak,T,CKata);
+    if(x == false){
+        printf("Not enought money!\n");
+        BacaInput();
+    }else{
+        //masuk ke stack
+    //    TambahMenit(&J,10);
+    //    TulisJam(J);
+     printf("BENER");
+    }
+    return 0;
+}
+
