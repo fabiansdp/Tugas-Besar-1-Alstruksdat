64a58628b4b2b65a727ff2d647fb63f0e1ffea5e
Purecon <mfurqon2001@gmail.com>
2020-11-24 09:49:30 +0700

    Edit main, edit MaxEl MesinKata

--- src/main.c
+++ src/main.c
@@ -81,136 +81,136 @@ int main()
         read_kata_start();
     }
     
-    // //peta
-    // //set matriks tipe
-    // for (i = 0; i <= 9; i++)
+    //peta
+    //set matriks tipe
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 19; j++)
+        {
+            tipe_point[i][j]=0;
+        }
+    }
+
+    //set matriks peta
+    MakeMATRIKS(10,20,&L);
+    BacaPeta(1,&L);
+    TulisPeta(1,L);
+    TitikPeta(L,AP); //set titik pada peta
+
+    //cek titik
+    // for (i = 0; i <= 199; i++)
     // {
-    //     for (j = 0; j <= 19; j++)
+    //     if (AP.P[i].X!=0 && AP.P[i].Y!=0)
     //     {
-    //         tipe_point[i][j]=0;
-    //     }
+    //         printf("%d ",AP.tipe[i]);
+    //         printf("%f,",AP.P[i].X);
+    //         printf("%f\n",AP.P[i].Y);
+    //     }           
     // }
 
-    // //set matriks peta
-    // MakeMATRIKS(10,20,&L);
-    // BacaPeta(1,&L);
-    // TulisPeta(1,L);
-    // TitikPeta(L,AP); //set titik pada peta
+    printf("X untuk keluar\n");
+    loop=true;    
+    do
+    {
+        adjacent=Adjacency();
+        printf("Nilai adjacent: %d\n",adjacent);
+        printf("Masukkan arah pergerakan:\n");
+        scanf("%c",&input);
+        if (adjacent==5)
+        {
+            printf("Sebelah wahana\n");
+        }
+        else if (adjacent==3)
+        {
+            printf("Sebelah office\n");
+        }
+        if (input=='X')
+        {
+            loop=false;
+        }
+        else if (input=='W'||input=='A'||input=='S'||input=='D')
+        {
+            Movement(input,&L);
+            PrintPeta(L);
+        }
+    } while (loop);
 
-    // //cek titik
-    // // for (i = 0; i <= 199; i++)
-    // // {
-    // //     if (AP.P[i].X!=0 && AP.P[i].Y!=0)
-    // //     {
-    // //         printf("%d ",AP.tipe[i]);
-    // //         printf("%f,",AP.P[i].X);
-    // //         printf("%f\n",AP.P[i].Y);
-    // //     }           
-    // // }
+    //cek tipe_point
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 19; j++)
+        {
+            printf("%d",tipe_point[i][j]);
+        }
+        printf("\n");
+    }
+    //antrian
+    MakeEmpty(&Q, MaxAntrian);
+    while (!IsFullQueue(Q))
+    {
+        /*Inisialisasi infotype_pq*/
+        srand(crntname); //lebih baik nanti pake ADT time
+        Nama(temp)=Nama[crntname];
+        Prio(temp)=crntname;
+        Kesabaran(temp)=(rand()%3)+1;
+        // Kesabaran(temp)=default_angka;
+        /*Selama tidak full, bisa tambah antrian*/
+        TambahAntrian(&Q,temp);
+        /*Setiap bertambah waktu tambah antrian(not implemented)*/
+        crntname++; /*Nama selanjutnya*/
+        if (crntname==27)
+        {
+            crntname=0;
+        }
+    }
+    printf("Antrian awal\n");
+    PrintAntrian(Q);
 
-    // printf("X untuk keluar\n");
-    // loop=true;    
-    // do
-    // {
-    //     adjacent=Adjacency();
-    //     printf("Nilai adjacent: %d\n",adjacent);
-    //     printf("Masukkan arah pergerakan:\n");
-    //     scanf("%c",&input);
-    //     if (adjacent==5)
-    //     {
-    //         printf("Sebelah wahana\n");
-    //     }
-    //     else if (adjacent==3)
-    //     {
-    //         printf("Sebelah office\n");
-    //     }
-    //     if (input=='X')
-    //     {
-    //         loop=false;
-    //     }
-    //     else if (input=='W'||input=='A'||input=='S'||input=='D')
-    //     {
-    //         Movement(input,&L);
-    //         PrintPeta(L);
-    //     }
-    // } while (loop);
+    //Cek wahana yang ingin dinaiki
+    int nama=0;
+    char ch; 
+    ch = Wahana(ElmtQ(Q,0),nama);
+    printf("Wahana pelanggan %c: \n",Nama(ElmtQ(Q,0)));
+    while (ch !='.')
+    {
+        printf("%c",ch);
+        nama++;
+        ch = Wahana(ElmtQ(Q,0),nama);
+    }
+    printf("\n");
 
-    // //cek tipe_point
-    // for (i = 0; i <= 9; i++)
-    // {
-    //     for (j = 0; j <= 19; j++)
-    //     {
-    //         printf("%d",tipe_point[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-    // //antrian
-    // MakeEmpty(&Q, MaxAntrian);
-    // while (!IsFullQueue(Q))
-    // {
-    //     /*Inisialisasi infotype_pq*/
-    //     srand(crntname); //lebih baik nanti pake ADT time
-    //     Nama(temp)=Nama[crntname];
-    //     Prio(temp)=crntname;
-    //     Kesabaran(temp)=(rand()%3)+1;
-    //     // Kesabaran(temp)=default_angka;
-    //     /*Selama tidak full, bisa tambah antrian*/
-    //     TambahAntrian(&Q,temp);
-    //     /*Setiap bertambah waktu tambah antrian(not implemented)*/
-    //     crntname++; /*Nama selanjutnya*/
-    //     if (crntname==27)
-    //     {
-    //         crntname=0;
-    //     }
-    // }
-    // printf("Antrian awal\n");
-    // PrintAntrian(Q);
+    nama=0;
+    ch = Wahana(ElmtQ(Q,2),nama);
+    printf("Wahana pelanggan %c: \n",Nama(ElmtQ(Q,2)));
+    while (ch !='.')
+    {
+        printf("%c",ch);
+        nama++;
+        ch = Wahana(ElmtQ(Q,2),nama);
+    }
+    printf("\n");
+    printf("Antrian[%d/%d]",NBElmtQueue(Q),MaxAntrian);
 
-    // //Cek wahana yang ingin dinaiki
-    // int nama=0;
-    // char ch; 
-    // ch = Wahana(ElmtQ(Q,0),nama);
-    // printf("Wahana pelanggan %c: \n",Nama(ElmtQ(Q,0)));
-    // while (ch !='.')
-    // {
-    //     printf("%c",ch);
-    //     nama++;
-    //     ch = Wahana(ElmtQ(Q,0),nama);
-    // }
-    // printf("\n");
-
-    // nama=0;
-    // ch = Wahana(ElmtQ(Q,2),nama);
-    // printf("Wahana pelanggan %c: \n",Nama(ElmtQ(Q,2)));
-    // while (ch !='.')
-    // {
-    //     printf("%c",ch);
-    //     nama++;
-    //     ch = Wahana(ElmtQ(Q,2),nama);
-    // }
-    // printf("\n");
-    // printf("Antrian[%d/%d]",NBElmtQueue(Q),MaxAntrian);
-
-    // //Simulasi antrian habis dari penuh
-    // while (!IsEmptyQueue(Q))
-    // {
-    //     KurangAntrian(&Q,&served);
-    //     printf("Pelanggan %c dilayani\n",Nama(served));
-    //     printf("Antrian[%d/%d]",NBElmtQueue(Q),MaxAntrian);
-    //     if (!IsEmptyQueue(Q))
-    //     {
-    //         PrintAntrian(Q);
-    //     }
-    // }
-    // //stack command
-    // Comm(comm1) = 1;
-    // Comm(comm2) = 2;
-    // Comm(comm3) = 3;
-    // Comm(comm4) = 4;
-    // Push(&S, comm1);
-    // Push(&S, comm2);
-    // Push(&S, comm3);
-    // Push(&S, comm4);
-    // execute(&S);
+    //Simulasi antrian habis dari penuh
+    while (!IsEmptyQueue(Q))
+    {
+        KurangAntrian(&Q,&served);
+        printf("Pelanggan %c dilayani\n",Nama(served));
+        printf("Antrian[%d/%d]",NBElmtQueue(Q),MaxAntrian);
+        if (!IsEmptyQueue(Q))
+        {
+            PrintAntrian(Q);
+        }
+    }
+    //stack command
+    Comm(comm1) = 1;
+    Comm(comm2) = 2;
+    Comm(comm3) = 3;
+    Comm(comm4) = 4;
+    Push(&S, comm1);
+    Push(&S, comm2);
+    Push(&S, comm3);
+    Push(&S, comm4);
+    execute(&S);
     return 0;
 }
