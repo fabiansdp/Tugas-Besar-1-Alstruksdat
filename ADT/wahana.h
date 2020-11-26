fed812347dcd45011cae44f8f7172a10e382552e
pratamaandiko <pratamaandiko@gmail.com>
2020-11-26 18:17:31 +0700

    add fungsi namaToID, searchNamaWahana to wahana.c

--- ADT/wahana.h
+++ ADT/wahana.h
@@ -13,20 +13,22 @@
 /* Indeks maksimum array */
 #define IdxMin 0
 /* Indeks minimum array */
-#define IdxUndef -999 
+#define IdxUndef -999
 /* Indeks tak terdefinisi*/
 #define ValUndef -1
 /* Nilai elemen tak terdefinisi*/
 
 /* Struktur buat Laporan Wahana*/
-typedef struct {
+typedef struct
+{
     int air;
     int kayu;
     int batu;
     int besi;
 } UpgradeMaterial;
 
-typedef struct {
+typedef struct
+{
     int naiktotal;
     int penghasilantotal;
     int naikharian;
@@ -34,7 +36,8 @@ typedef struct {
 } Laporan;
 
 /* Struktur Data Khusus buat Detail Wahana */
-typedef struct {
+typedef struct
+{
     int id, harga, kapasitas, durasi, status;
     boolean isDibangun;
     POINT lokasi;
@@ -44,27 +47,27 @@ typedef struct {
     UpgradeMaterial material;
 } Wahana;
 
-typedef struct {
-    Wahana ArrayW[IdxMax+1];
+typedef struct
+{
+    Wahana ArrayW[IdxMax + 1];
     int jumlahWahana;
 } ArrayWahana;
 
-#define ID(W,i) (W).ArrayW[(i)].id
-#define Harga(W,i) (W).ArrayW[(i)].harga
-#define Kapasitas(W,i) (W).ArrayW[(i)].kapasitas
-#define Durasi(W,i) (W).ArrayW[(i)].durasi
-#define Status(W,i) (W).ArrayW[(i)].status
-#define NaikTotal(W,i) (W).ArrayW[(i)].laporan.naiktotal
-#define TotalPenghasilan(W,i) (W).ArrayW[(i)].laporan.penghasilantotal
-#define NaikHarian(W,i) (W).ArrayW[(i)].laporan.naikharian
-#define PenghasilanHarian(W,i) (W).ArrayW[(i)].laporan.penghasilanharian
-#define IsDibangun(W,i) (W).ArrayW[(i)].isDibangun
-#define Air(W,i) (W).ArrayW[(i)].material.air
-#define Kayu(W,i) (W).ArrayW[(i)].material.kayu
-#define Batu(W,i) (W).ArrayW[(i)].material.batu
-#define Besi(W,i) (W).ArrayW[(i)].material.besi
+#define ID(W, i) (W).ArrayW[(i)].id
+#define Harga(W, i) (W).ArrayW[(i)].harga
+#define Kapasitas(W, i) (W).ArrayW[(i)].kapasitas
+#define Durasi(W, i) (W).ArrayW[(i)].durasi
+#define Status(W, i) (W).ArrayW[(i)].status
+#define NaikTotal(W, i) (W).ArrayW[(i)].laporan.naiktotal
+#define TotalPenghasilan(W, i) (W).ArrayW[(i)].laporan.penghasilantotal
+#define NaikHarian(W, i) (W).ArrayW[(i)].laporan.naikharian
+#define PenghasilanHarian(W, i) (W).ArrayW[(i)].laporan.penghasilanharian
+#define IsDibangun(W, i) (W).ArrayW[(i)].isDibangun
+#define Air(W, i) (W).ArrayW[(i)].material.air
+#define Kayu(W, i) (W).ArrayW[(i)].material.kayu
+#define Batu(W, i) (W).ArrayW[(i)].material.batu
+#define Besi(W, i) (W).ArrayW[(i)].material.besi
 #define JumlahWahana(W) (W).jumlahWahana
-
 
 // Baca dari teks file
 void ListWahana();
@@ -87,5 +90,11 @@ void InfoWahana(ArrayWahana W, int id);
 // Laporan Wahana by ID
 void LaporanWahana(ArrayWahana W, int id);
 
-#endif
+void namaToID(Kata nama, int *idwahana);
+/* I.S. nama wahana benar dan terdefinisi di wahana.txt */
+/* F.S. idwahana berisi id dari nama*/
 
+boolean searchNamaWahana(Kata nama);
+/* Mengirimkan true jika terdapat nama di wahana.txt */
+
+#endif
